5e86df1c18610fe2d41bb16d7cefb1af1c4a8e0d
jess-rae-miller-lab102 <136469171+jess-rae-miller@users.noreply.github.com>
2024-02-16 15:54:27 +1100

    player movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,18 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float speed = 10f;
 
-    // Update is called once per frame
+    Vector2 lastClickedPos;
+
+    bool moving;
+
     void Update()
     {
+        if (Input.GetMouseButtonDown(0))
+        {
+            lastClickedPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            moving = true;
+        }
         
+        if (moving && (Vector2)transform.position != lastClickedPos)
+        {
+            float step = speed * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position, lastClickedPos, step);
+        }
+        else
+        {
+            moving = false;
+        }
     }
 }
