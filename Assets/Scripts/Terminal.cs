f1780463e940f6bd26546b6e51c9cf7e1014401f
jess-rae-miller-lab102 <136469171+jess-rae-miller@users.noreply.github.com>
2024-02-23 09:09:51 +1100

    last minute terminal fix

--- Assets/Scripts/Terminal.cs
+++ Assets/Scripts/Terminal.cs
@@ -1,35 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
-public class Terminal : MonoBehaviour
+public class Terminal : ItemData
 {
     public bool isUnlocked = false;
     public GameObject codes;
     public GameObject unlockedObject;
-    public GameObject turretToDeactivate; // Reference to the turret GameObject to deactivate
 
     private void Start()
     {
         unlockedObject.SetActive(false);
     }
-
     public void UnlockTerminal()
     {
         isUnlocked = true;
-        // Activate the unlocked object
-        unlockedObject.SetActive(true);
-
-        // Check if the turret GameObject reference is valid
-        if (turretToDeactivate != null)
-        {
-            // Deactivate the turret GameObject
-            turretToDeactivate.SetActive(false);
-        }
-        else
-        {
-            Debug.LogWarning("Turret to deactivate is not assigned to the Terminal.");
-        }
+        objectsToAdd.Add(unlockedObject);
     }
-
     private void Update()
     {
         if (codes == null)
